e5bf668e6890de17d390ad1e137f520cd0347ad4
michelangelo17 <michelangelo.markus@gmail.com>
2020-05-18 14:04:10 -0700

    day one MVP complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,15 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +21,30 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = mdr
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +56,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +70,25 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        # program commands
+        ldi = 0b10000010
+        prn = 0b01000111
+        hlt = 0b00000001
+
+        self.load()
+
+        halt = False
+
+        while halt is not True:
+            instruction = self.ram_read(self.pc)
+            if instruction == hlt or self.pc > 10:
+                halt = True
+            elif instruction == ldi:
+                self.reg[self.ram_read(self.pc+1)] = self.ram_read(self.pc+2)
+                self.pc += 3
+            elif instruction == prn:
+                print(self.reg[self.ram_read(self.pc+1)])
+                self.pc += 2
+            else:
+                return print(f'Instruction {instruction} not found at {self.pc}')
