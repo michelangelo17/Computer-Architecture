b4dabbbb35203b2ca3ca41c2f3b02b4d742830ac
michelangelo17 <michelangelo.markus@gmail.com>
2020-05-19 18:33:03 -0700

    cleaner implementation

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -12,16 +12,6 @@ class CPU:
         self.reg = [0] * 8
         self.pc = 0
         self.reg[7] = 0xF4
-        self.pc_increase = 0
-        self.commands = {
-            0b10000010: self.ldi,
-            0b01000111: self.prn,
-            0b00000000: self.nop,
-            0b01000101: self.push,
-            0b01000110: self.pop,
-            0b01010000: self.call,
-            0b00010001: self.ret,
-        }
 
     def load(self):
         """Load a program into memory."""
@@ -111,11 +101,9 @@ class CPU:
     def call(self):
         self.push(self.pc+2)
         self.pc = self.reg[self.ram_read(self.pc+1)]
-        self.pc_increase = 0
 
     def ret(self):
         self.pc = self.pop(True)
-        self.pc_increase = 0
 
     def checkAlu(self, instruction):
         is_alu = (instruction & 0b00100000) >> 5
@@ -128,17 +116,32 @@ class CPU:
 
         return instruction
 
+    def pc_inc_cal(self, instruction):
+        if (instruction & 0b00010000) >> 4 != 1:
+            return (instruction >> 6) + 1
+        return 0
+
     def run(self):
         """Run the CPU."""
 
+        commands = {
+            0b10000010: self.ldi,
+            0b01000111: self.prn,
+            0b00000000: self.nop,
+            0b01000101: self.push,
+            0b01000110: self.pop,
+            0b01010000: self.call,
+            0b00010001: self.ret,
+        }
+
         while self.ram_read(self.pc) != 0b00000001 and self.pc < len(self.ram) - 1:
             instruction = self.ram_read(self.pc)
-            self.pc_increase = (instruction >> 6) + 1
+            pc_increase = self.pc_inc_cal(instruction)
             instruction = self.checkAlu(instruction)
 
             try:
-                self.commands[instruction]()
-                self.pc += self.pc_increase
+                commands[instruction]()
+                self.pc += pc_increase
 
             except KeyError:
                 return print(f'\nInstruction {instruction} not found at {self.pc}\n')
